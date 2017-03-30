abcfb33cdb516ca8f4dedc17a532e62626901fee
Davide Di Mauro <davidedimauro88@gmail.com>
2017-03-30 10:58:10 +0100

    change spec file's name

--- test/integration/packer/serverspec/localhost/packer_spec.rb
+++ test/integration/packer/serverspec/localhost/packer_spec.rb
@@ -4,6 +4,7 @@ set :backend, :exec
 describe file('/usr/local/bin/packer') do
     it { should exist }
     it { should be_executable.by('others') }
+    it { should be_linked_to '/var/lib/packer' }
 end
 
 describe file('/tmp/packer_0.10.1_linux_amd64.zip') do
